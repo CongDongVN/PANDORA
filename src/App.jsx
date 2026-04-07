a7d2c13b2bc7eb19224c529dc3a067e97f9999b4
YinML1205 <ngaoan64@gmail.com>
2026-04-07 22:00:03 +0700

    trang gio hang

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Cart from './pages/cart'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
